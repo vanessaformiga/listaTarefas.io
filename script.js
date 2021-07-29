8dd1b18c5ccbb76d532ea70ce65b8bf12236bc93
vanessaformiga <vanessandrade16@gmail.com>
2021-07-29 14:49:02 -0300

    criado repositório

---
+++ script.js
@@ -0,0 +1,76 @@
+const listaUl = document.querySelector('ul')
+const inputTarefa = document.querySelector('input')
+const inputButton = document.querySelector('button')
+
+
+const tarefas = JSON.parse(localStorage.getItem('list_tarefas')) || []
+
+function listarTarefas() {
+
+    listaUl.innerHTML = ''
+
+    for (tarefa of tarefas) {
+
+
+        const criarLista = document.createElement('li')
+        const taskTarefa = document.createTextNode(tarefa)
+
+        const elementoLink = document.createElement('a')
+        const posicao = tarefas.indexOf(tarefa)
+
+        const inserirX = document.createTextNode('X')
+        elementoLink.appendChild(inserirX)
+
+        elementoLink.setAttribute('href', '#')
+        elementoLink.setAttribute('onclick', `deletarTarefa(${posicao})`)
+
+
+        criarLista.appendChild(taskTarefa)
+        listaUl.appendChild(criarLista)
+        criarLista.appendChild(elementoLink)
+
+
+
+    }
+}
+
+
+listarTarefas()
+
+
+function adicionarTarefa() {
+
+
+
+    const taskTarefa = inputTarefa.value
+    tarefas.push(taskTarefa)
+    inputTarefa.value = ''
+
+    listarTarefas()
+
+
+    salvarNoLocalStorage()
+
+
+
+}
+
+inputButton.setAttribute('onclick', 'adicionarTarefa()')
+
+function deletarTarefa(posicao) {
+    tarefas.splice(posicao, 1)
+
+
+    listarTarefas()
+
+
+    salvarNoLocalStorage()
+}
+
+
+
+function salvarNoLocalStorage() {
+    localStorage.setItem('list_tarefas', JSON.stringify(tarefas))
+
+
+}
